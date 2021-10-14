42125e37de0400239b792ed770a8af3951045ef7
郭敏 <422859608@qq.com>
2021-10-14 14:09:51 +0800

    commit

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -26,6 +26,9 @@ Page({
       });
     }
   },
+  getUserInfo(value){
+    console.log(value,'value')
+  },
   getUserProfile: function getUserProfile(e) {
     var _this = this;
 
@@ -43,12 +46,5 @@ Page({
       }
     });
   },
-  getUserInfo: function getUserInfo(e) {
-    // 不推荐使用getUserInfo获取用户信息，预计自2021年4月13日起，getUserInfo将不再弹出弹窗，并直接返回匿名的用户个人信息
-    console.log(e);
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    });
-  }
+
 });
